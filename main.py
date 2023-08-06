ec9c7255dcef39389226879c2c8a66390f60fd99
Benjamin Fuller <benisfull@gmail.com>
2023-08-05 23:46:06 -0700

    add quit, adjust movement, add gameloop

--- main.py
+++ main.py
@@ -20,17 +20,45 @@ class Tadpole(pg.sprite.Sprite):
         self.image.fill(WHITE)
         self.rect = self.image.get_rect()
         self.rect.center = (screen_width // 2, screen_height // 2)
-        self.speed = 5
+        self.speed = 1
 
     def update(self):
         # Get the keys pressed
         keys = pg.key.get_pressed()
         # Update the position based on the keys pressed
-        if keys[pg.K_LEFT]:
-            self.rect.x -= self.speed
-        if keys[pg.K_RIGHT]:
-            self.rect.x += self.speed
-        if keys[pg.K_UP]:
+        if keys[pg.K_q]:
+            pg.quit()
+            sys.exit()
+        if keys[pg.K_w]:
             self.rect.y -= self.speed
-        if keys[pg.K_DOWN]:
+        if keys[pg.K_a]:
+            self.rect.x -= self.speed
+        if keys[pg.K_s]:
             self.rect.y += self.speed
+        if keys[pg.K_d]:
+            self.rect.x += self.speed
+
+
+def main():
+    # Create the tadpole object
+    tadpole = Tadpole()
+    all_sprites = pg.sprite.Group(tadpole)
+
+    # Game loop
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+
+        # Update tadpole position
+        all_sprites.update()
+
+        # Draw everything
+        screen.fill((0, 0, 0))  # Clear the screen
+        all_sprites.draw(screen)  # Draw the tadpole
+        pg.display.flip()  # Update the display
+
+
+if __name__ == "__main__":
+    main()
