b905130c3a14b17f44f651a55b57876d975dc7c2
Benjamin Fuller <benisfull@gmail.com>
2024-02-28 10:55:39 -0800

    movement update + OOP

--- main.py
+++ main.py
@@ -15,51 +15,66 @@ class Player(pg.sprite.Sprite):
         super().__init__()
         self.image = pg.Surface((20, 20))
         self.image.fill(WHITE)
-        self.position = [1, 1]
         self.rect = self.image.get_rect()
-        self.rect.center = (self.position[0] * tile, self.position[1] * tile)
-        self.speed = 250
+        self.rect.center = (1 * tile, 1 * tile)
+        self.direction = Vector2()
+        self.position = Vector2(self.rect.center)
+        self.last_position = self.position
+        self.is_moving = False
+        self.speed = 150
 
-    def update(self, dt):
-        """
+    def input(self):
+        keys = pg.key.get_pressed()
+        self.is_moving = False
 
-        :param dt:
-        :return:
-        """
+        # direction handling
+        # [WASD] keys for player direction
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-            if event.type == pg.KEYDOWN:  # key mapping
-                # controls: WASD for move, SPACE for stop/resume, q for quit
-                # SPACE will stop and resume movement and inputting WASD after stopping will also resume
-                if event.key == pg.K_w:
-                    # North
-                    self.position[1] -= 1
-                elif event.key == pg.K_d:
-                    # East
-                    self.position[0] += 1
-                elif event.key == pg.K_s:
-                    # South
-                    self.position[1] += 1
-                elif event.key == pg.K_a:
-                    # West
-                    self.position[0] -= 1
+            elif event.type == pg.KEYDOWN:
                 if event.key == pg.K_q:
                     pg.quit()  # quit on keystroke q
                     sys.exit()
 
-                if 0 <= self.position[1] <= screen_width - self.rect.width:
-                    self.rect.x = self.position[0] * tile
+        if keys[pg.K_w]:
+            self.direction.y = -1
+            self.is_moving = True
+        elif keys[pg.K_s]:
+            self.direction.y = 1
+            self.is_moving = True
+        else:
+            self.direction.y = 0
 
-                if 0 <= self.position[0] <= screen_height - self.rect.height:
-                    self.rect.y = self.position[1] * tile
+        if keys[pg.K_a]:
+            self.direction.x = -1
+            self.is_moving = True
+        elif keys[pg.K_d]:
+            self.direction.x = 1
+            self.is_moving = True
+        else:
+            self.direction.x = 0
 
+    def move(self, dt):
+        if self.direction.magnitude() > 0:
+            self.direction = self.direction.normalize()
+        movement = self.direction * self.speed * dt
+
+        self.last_position = self.position - movement
+        self.position += movement
+
+        # set center position of player rect to the location of the position vector
+        self.rect.center = round(self.position)
+
+    def update(self, dt):
+        self.input()
+        self.move(dt)
 
 
 class Game:
     def __init__(self):
-        self.previous_time = time.time()  # create clock for calculating delta time
+        self.previous_time = time.time()  # start initial instance of timer for calculating delta time
         self.player = Player()
 
     def main(self):
